331b7c68ab602e0dbd19d51036d35a102a20d0bf
Juan Jose Morales <juanjoseotero993@gmail.com>
2026-05-12 09:37:14 -0500

    ProductoPt3

--- app/src/main/java/com/example/invenzo_10/MainActivity.kt
+++ app/src/main/java/com/example/invenzo_10/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.invenzo_10
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +21,16 @@ class MainActivity : AppCompatActivity() {
         inicioSecion.setOnClickListener {
             val intent = Intent(this, ActivityInicio::class.java)
             startActivity(intent)
+        }
+        val bottomNavigationView = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.post {
+            bottomNavigationView.selectedItemId = R.id.more
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.recyclerProductos)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
 
     }
