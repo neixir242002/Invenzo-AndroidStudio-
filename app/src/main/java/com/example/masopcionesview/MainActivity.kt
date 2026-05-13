9242fcf00d799ef4d46bfd5816545d0a52436a6b
Neixir <palaciosmateo080@gmail.com>
2026-05-13 08:00:54 -0500

    Cambio de nombre de xml activity_main a activity_masopciones

--- app/src/main/java/com/example/masopcionesview/MainActivity.kt
+++ app/src/main/java/com/example/masopcionesview/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_masopciones)
 
         val bottomNavigationView = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.post {
