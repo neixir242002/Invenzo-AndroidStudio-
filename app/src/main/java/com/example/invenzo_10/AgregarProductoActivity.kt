f117a6ec8f1df8814b4787b0a42424dd494db37e
Juan Jose Morales <juanjoseotero993@gmail.com>
2026-05-13 11:57:48 -0500

    Producto Agregar

--- app/src/main/java/com/example/invenzo_10/AgregarProductoActivity.kt
+++ app/src/main/java/com/example/invenzo_10/AgregarProductoActivity.kt
@@ -1,4 +1,55 @@
 package com.example.invenzo_10
 
-class AgregarProductoActivity {
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class AgregarProductoActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_agregar_producto)
+
+        // Bottom Navigation
+//        val bottomNav =
+//            findViewById<BottomNavigationView>(
+//                R.id.bottomNav
+//            )
+//
+//        bottomNav.setOnItemSelectedListener { item ->
+//
+//            when (item.itemId) {
+//
+//                R.id.home -> {
+//
+//                    finish()
+//
+//                    true
+//                }
+//
+//                R.id.products -> {
+//
+//                    true
+//                }
+//
+//                R.id.reports -> {
+//
+//                    true
+//                }
+//
+//                R.id.more -> {
+//
+//                    true
+//                }
+//
+//                else -> false
+//            }
+//        }
+    }
 }
